5a7990d075fcab45c9c4049d486a98077e572e9c
ad-dw <aditya_dwivedi01@outlook.com>
2025-06-15 12:29:27 +0530

    Implementing bookmark functionality

--- src/js/model.js
+++ src/js/model.js
@@ -8,12 +8,17 @@ export const state = {
     searchResults: [],
     currentPage: 1,
   },
+  bookmarks: [],
 };
 
 export const loadRecipe = async (id) => {
   try {
     let data = await getJSON(`${API_URL}/${id}?`);
     state.recipe = data.recipe;
+    if (state.bookmarks.some((bmk) => bmk.id === state.recipe.id))
+      state.recipe.bookmarked = true;
+    else state.recipe.bookmarked = false;
+    console.log(state.recipe);
   } catch (err) {
     throw err;
   }
@@ -22,6 +27,7 @@ export const loadRecipe = async (id) => {
 export const loadSearchResults = async (query) => {
   try {
     state.search.query = query;
+    state.search.currentPage = 1;
     let data = await getJSON(`${API_URL}?search=${query}&`);
     state.search.searchResults = data.recipes;
   } catch (err) {
@@ -44,4 +50,15 @@ export const updateServingsData = function (newServing) {
       (ing.quantity = (ing.quantity / state.recipe.servings) * newServing)
   );
   state.recipe.servings = newServing;
+};
+
+export const addBookmark = function () {
+  state.recipe.bookmarked = true;
+  state.bookmarks.push(state.recipe);
+};
+
+export const removeBookmark = function (id) {
+  state.recipe.bookmarked = false;
+  const index = state.bookmarks.findIndex((el) => el.id === id);
+  state.bookmarks.splice(index, 1);
 };
